bcfaa9537e4c8eaf73e30f39ab9264b1aeed9f1d
Josh Skillings <jskillings@gmail.com>
2019-10-14 13:41:15 -0600

    started 3.7

---
+++ src/QuizQuestion.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class QuizQuestion extends Component {
+
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>// insrtuction text goes here</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <p>quiz question button logic goes here</p>
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestions
